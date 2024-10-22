fd013b121f21a87645315b27fef737d7470cc1aa
LuckySoftware <99148157+LuckySoftware@users.noreply.github.com>
2024-10-21 22:05:24 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto_2do_isbo/BaseDeDatos.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_2do_isbo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*
+ *
+ * @author LucianoMoreira
+ *
+ */
+
+public class BaseDeDatos 
+{
+        // PARAMETROS DE LA BASE DE DATOS (DB)
+        private final String url = "jdbc:mysql://localhost:4306/proyecto_db";
+        private final String usuario = "root";
+        private final String pass = "";
+        
+        
+        // PROPIEDADES DE LA CLASE
+        private Connection connection;
+        private ResultSet result;
+        
+        
+        // METODO CONSTRUCTUR DE LA CLASE
+        public BaseDeDatos() 
+        {
+            
+            try
+            {
+                this.connection = DriverManager.getConnection(url, usuario, pass);
+                        
+            }
+            
+            catch(SQLException ex)
+            {
+                JOptionPane.showMessageDialog(null,  "Error: " + ex.getMessage());
+            }
+            
+            
+            this.result = null;
+        }
+        
+        
+        // METODO PARA EJECUTAR CONSULTAS SELECT
+        public void ejecutarSQL(PreparedStatement sql)
+        {
+            
+            try
+            {
+                
+                this.result = sql.executeQuery();
+                
+            }
+            
+            
+            catch(SQLException ex)
+            {
+                
+                JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+                this.result = null;  
+                
+            }
+        }
+        
+         //Método para ejecutar consultas de tipo Ingreso, modificación o borrado
+         public void ejecutar(PreparedStatement sql)
+         {
+            try{
+                sql.execute();
+            }catch(SQLException ex)
+            {
+                JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+            }
+        }
+        
+
+        // GETTERS & SETTERS
+        public Connection getConnection() 
+        {
+            return connection;
+        }
+
+        
+        public void setConnection(Connection connection) 
+        {
+            this.connection = connection;
+        }
+
+        
+        public ResultSet getResult() 
+        {
+            return result;
+        }
+
+        
+        public void setResult(ResultSet result) 
+        {
+            this.result = result;
+        }
+}
---
+++ src/main/java/com/mycompany/proyecto_2do_isbo/PROYECTO_2do_ISBO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_2do_isbo;
+
+/**
+ *
+ * @author ilucky
+ */
+public class PROYECTO_2do_ISBO {
+
+    public static void main(String[] args) 
+    {
+        // VENTANA DE INICIO DE SESION
+        Inicio ventanaInicio = new Inicio();
+        ventanaInicio.setVisible(true);
+        
+        Registro ventanaRegistro = new Registro();
+        ventanaRegistro.setVisible(false);
+    }
+}
